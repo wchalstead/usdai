7b21ad88e2bdfde5aeb49d298ff3aaecbf99abc8
Wesley Halstead <wesley.halstead@tamu.edu>
2025-11-20 14:56:32 -0600

    Distribution function.

---
+++ R/distribution.R
@@ -0,0 +1,19 @@
+qcrossW <- function(p){
+  # Check input of p
+  if( any(p > 1) | any(p < 0)){
+    stop("p must be in [0, 1]")
+  }
+
+  # Return vectorized list of quantile values
+  results <- sapply(p, \(p) {
+    if(p == 1){
+      return(Inf)
+    } else if (p == 0) {
+      return(0)
+    } else {
+    return(quantile(DATASET, p))
+    }}
+  )
+
+  return(unname(results))
+}
