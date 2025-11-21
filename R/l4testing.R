0032d5e491141285fb771dd052da335f8f266277
Wesley Halstead <wesley.halstead@tamu.edu>
2025-11-21 09:23:38 -0600

    Updated documentation for L4 testing.

--- R/l4testing.R
+++ R/l4testing.R
@@ -101,15 +101,27 @@ crossUStatL4_cum <- function(data, m) {
 # Calculate self-normalized value using cumulative stats
 # data is nxp matrix
 # m is integer cross "threshold" value
-#' Title
+#' The Self-Normalized Cross U-Statistic for \eqn{\ell_4} Norm Testing
 #'
-#' @param data
-#' @param m
+#' @param data \eqn{n \times p} matrix of data.
+#' @param m Cross threshold value. Should be between 3 and \eqn{n}.
 #'
-#' @returns
+#' @returns The self-normalized cross U-Statistic for the \eqn{\ell_4}-norm
+#' The cross U-Statistic is given as \deqn{
+#' \widetilde \theta_{(m+1):k} = \sum_{1 \leq i_1,...,i_{3} \leq m}^{*}\sum_{j = m+1}^k\left(\sum_{l = 1}^pX_{i_1,l}X_{i_2,l}X_{i_{3},l}X_{j,l}\right)
+#' }, the self normalizer is given as \deqn{
+#' V_{(m+1):n} = \frac{1}{n-m}\sum_{k = m+1}^n\left(\widetilde \theta_{(m+1):k} - \frac{k-m}{n-m}\widetilde \theta_{(m+1):n}\right)^2.
+#' }, and the self normalized statistic is given as \deqn{
+#' W := \frac{\widetilde \theta_{(m+1):n}^2}{V_{(m+1):n}}
+#' }
 #' @export
 #'
 #' @examples
+#' set.seed(12)
+#' # Generate Data
+#' data <- matrix(rnorm(100 * 2), 100, 2)
+#' # Calculate U-Statistic
+#' crossWStatL4(data, 50)
 crossWStatL4 <- function(data, m){
   n <- nrow(data)
 
