45252a92add343d5041bbd991b7d6fafb906d222
Wesley Halstead <wesley.halstead@tamu.edu>
2025-12-07 21:24:13 -0600

    Initialize Cpp compatibility

---
+++ R/usdai-package.R
@@ -0,0 +1,9 @@
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @useDynLib usdai, .registration = TRUE
+## usethis namespace: end
+NULL
