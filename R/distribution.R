fc6c591eb101f208449f96bd120282b487326b83
Wesley Halstead <wesley.halstead@tamu.edu>
2025-12-13 16:09:37 -0600

    Write rcrossW function.

--- R/distribution.R
+++ R/distribution.R
@@ -33,3 +33,24 @@ pcrossW <- function(q){
   }
   )
 }
+
+rcrossW <- function(n, p = 200) {
+  # Check inputs
+  if(!is.numeric(n) || (n %% 1 != 0) ){
+    stop('n must be an integer')
+  }
+  if(!is.numeric(p) || (p %% 1 != 0) ){
+    stop('p must be an integer')
+  }
+
+  # Add one to adjust for numerator generation
+  p <- p + 1
+
+  Z <- matrix(rnorm(n * p), n, p)
+  Z0 <- Z[ , 1]
+  lambda <- matrix((pi * 1:(p - 1)), n, (p - 1), byrow = T)
+  Q <- rowSums((Z[ , -1] / lambda)^2)
+  results <- Z0^2/Q
+
+  return(results)
+}
